24d78e7f767c477ba525a7febe1c14477e7e86b6
Abdullah <abdullahisalam.lggs@gmail.com>
2025-11-07 22:35:41 +0500

    updated url

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,42 +5,42 @@ export default function Projects() {
     {
       title: "Calculator",
       desc: "A simple and responsive calculator built using HTML, CSS, and JavaScript.",
-      img: "/src/assets/calculator.png", // 👉 Add image link here
+      img: "/public/calculator.png", // 👉 Add image link here
       link: "https://abdullahislam123.github.io/calculator/",
       github: "https://github.com/abdullahislam123/calculator",
     },
     {
       title: "Unit Converter",
       desc: "A temperature and distance converter app with a clean and intuitive interface.",
-      img: "/src/assets//unit-converter.png",
+      img: "/public/unit-converter.png",
       link: "https://abdullahislam123.github.io/temperature-and-distance-converter/",
       github: "https://github.com/abdullahislam123/temperature-and-distance-converter",
     },
     {
       title: "Rock Paper Scissor",
       desc: "A fun interactive Rock Paper Scissors game made with HTML, CSS, and JS.",
-      img: "/src/assets/rock-paper-scissor.png",
+      img: "/public/rock-paper-scissor.png",
       link: "https://abdullahislam123.github.io/rock-papaer-scissor/",
       github: "https://github.com/abdullahislam123/rock-papaer-scissor",
     },
     {
       title: "Stop Watch",
       desc: "A digital stopwatch with start, stop, and reset functionality.",
-      img: "/src/assets/stop-watch.png",
+      img: "/public/stop-watch.png",
       link: "https://abdullahislam123.github.io/Stopwatch/",
       github: "https://github.com/abdullahislam123/Stopwatch",
     },
     {
       title: "Daily Expense Tracker",
       desc: "Track your daily expenses and manage your budget efficiently.",
-      img: "/src/assets/daily-expense-tracker.png",
+      img: "/public/daily-expense-tracker.png",
       link: "https://abdullahislam123.github.io/expense-tracker/",
       github: "https://github.com/abdullahislam123/smart-expense-manager",
     },
     {
       title: "Twitter Clone",
       desc: "A responsive Twitter clone featuring posts, likes, and UI interactions.",
-      img: "/src/assets/twitter.png",
+      img: "/public/twitter.png",
       link: "https://abdullahislam123.github.io/twitter-clone/",
       github: "https://github.com/abdullahislam123/twitter-clone",
     },
